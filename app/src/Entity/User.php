2fd2be6f26b1dac2540758c08f9b2b06769bd8db
Zhukov Sergei <alivesystemsonly@gmail.com>
2021-11-07 15:17:03 +0100

    Added firstname for user

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -30,6 +30,11 @@ class User implements UserInterface
      */
     private $roles = [];
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstName;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -111,5 +116,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
     }
 }
